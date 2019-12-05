7777e85f13c03ef34fb3a1af42c0cfabbdd4928c
BaronLip <baronlip@gmail.com>
2019-12-05 14:59:54 -0600

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,12 +1,21 @@
+import uuid from 'uuid';
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'ADD_TODO':
+      // When adding todo, creates a todo object with an unique ID using uuid function imported in.
+      const todo = {
+        id: uuid(),
+        text: action.payload.text
+      }
+      return { todos: state.todos.concat(todo) };
 
-      return { todos: state.todos.concat(action.payload.text) };
-
+    case 'DELETE_TODO':
+      // Using .filter, this returns all the todos that do not match our todo.
+      return { todos: state.todos.filter(todo => todo.id !== action.payload) }
     default:
       return state;
   }
